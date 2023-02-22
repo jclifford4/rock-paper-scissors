48ba927c71b95447ceb98143a8bb1ed60d9c6d6a
joseph <92230691+jclifford4@users.noreply.github.com>
2023-02-22 02:35:40 -0800

    Add event handlers to buttons

--- app.js
+++ app.js
@@ -1,57 +1,53 @@
 /*
 A function that generates and returns rock, paper, or scissors.
 */
-function getComputerChoice(numberChoices = 3){
+function getComputerChoice(numberChoices = 3) {
     const choices = ['Rock', 'Paper', 'Scissors'];
     // console.log(choices[Math.floor(Math.random() * numberChoices)]);
     return choices[Math.floor(Math.random() * numberChoices)];
 }
 
 // Returns the Player's Choice.
-function getPlayerChoice(){
+function getPlayerChoice(e) {
 
-    let playerSelection;
-    while ((playerSelection !== 'Rock' || 
-            playerSelection !== 'Paper' ||
-            playerSelection !== 'Scissors'))
-    {
-        if (playerSelection === 'Rock' || 
+    let playerSelection = e.target.innerText;
+    while ((playerSelection !== 'Rock' ||
+        playerSelection !== 'Paper' ||
+        playerSelection !== 'Scissors')) {
+        if (playerSelection === 'Rock' ||
             playerSelection === 'Paper' ||
-            playerSelection === 'Scissors')
-            {
-                return playerSelection;
-            }
-        playerSelection = prompt("Type 'Rock', 'Paper', or 'Scissors'!");
+            playerSelection === 'Scissors') {
+            return playerSelection;
+        }
+        // playerSelection = prompt("Type 'Rock', 'Paper', or 'Scissors'!");
     }
 
     return playerSelection;
-    
+    // return button.innerText;
+
 }
 
 // Logs The Score.
-function getScoreBoard(player, computer){
+function getScoreBoard(player, computer) {
     console.log(`Player: ${player} \tComputer: ${computer}`);
 }
 
 // Rock Beats Scissors
 // Scissors Beats Paper
 // Paper Beats Rock
-function generateRoundOutcome(playerSelection, computerSelection){
-    
-    if (playerSelection === computerSelection)
-    {
+function generateRoundOutcome(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
         console.log(`Tie, ${playerSelection} and ${computerSelection} Are The Same`);
         return -1;
     }
     else if (playerSelection === 'Rock' && computerSelection === 'Scissors'
         || (playerSelection === 'Paper' && computerSelection === 'Rock')
-        || (playerSelection === 'Scissors' && computerSelection === 'Paper'))
-    {
+        || (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
         console.log(`You Win ${playerSelection} Beats ${computerSelection}`);
         return 0;
     }
-    else
-    {
+    else {
         console.log(`You Lose ${playerSelection} Does Not Beat ${computerSelection}`);
         return 1;
     }
@@ -60,36 +56,55 @@ function generateRoundOutcome(playerSelection, computerSelection){
 }
 
 // Returns the 
-function getWinner(playerScore, computerScore){
+function getWinner(playerScore, computerScore) {
     if (playerScore > computerScore)
-        return 'Player';
+        return 'Player Wins!';
     else if (computerScore > playerScore)
-        return 'Computer';
+        return 'Computer Wins!';
     else
-        return "Nobody Wins";
+        return "Tie!";
 }
 
 
 
+
+
+
+const body = document.querySelector('#body');
+body.setAttribute('style', 'background: grey;');
+
+const resultDiv = document.createElement('div');
+resultDiv.classList.add('result');
+resultDiv.setAttribute('style', 'width: 200; height: 100;');
+
+// Query All the buttons
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button =>{ button.classList.add('Choicebutton')})
+
+console.log(buttons);
+buttons.forEach(button => 
+        button.addEventListener('click', playRound));
+
+
+
 // The Loop of the Game.
-function startGame(numberOfRounds = 5){
+function playRound(e) {
     var playerScore = 0;
     var computerScore = 0;
 
-    for (let i = 0; i < numberOfRounds; i++)
-    {
-        playerChoice = getPlayerChoice();
-        computerChoice = getComputerChoice();
+    playerChoice = getPlayerChoice(e);
+    computerChoice = getComputerChoice();
 
-        var roundWinner = generateRoundOutcome(playerChoice, computerChoice);
+    var roundWinner = generateRoundOutcome(playerChoice, computerChoice);
 
-        if (roundWinner === 0)
-            ++playerScore;
-        else if (roundWinner === 1)
-            ++computerScore;
-    
-    }
+    if (roundWinner === 0)
+        ++playerScore;
+    else if (roundWinner === 1)
+        ++computerScore;
 
     var gameWinner = getWinner(playerScore, computerScore);
     console.log(`The ${gameWinner} Wins!`);
+    resultDiv.textContent = `${gameWinner}`;
+    body.appendChild(resultDiv);
 }
